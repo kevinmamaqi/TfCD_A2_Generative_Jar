076fcdccec98761c26864ed482ee41019f6dbb76
Kevin Mamaqi <kevinmamaqi@gmail.com>
2017-12-18 00:12:31 +0100

    Help modifications

--- generative.js
+++ generative.js
@@ -5,9 +5,9 @@ const io 		= require('socket.io')(server);
 const jscad = require('@jscad/openjscad')
 const fs = require('fs')
 
-var scale 		= require('scale-number-range');
-var five 		= require("johnny-five"),
-    board 		= new five.Board();
+// var scale 		= require('scale-number-range');
+// var five 		= require("johnny-five"),
+//     board 		= new five.Board();
 
 
 
@@ -25,53 +25,53 @@ app.get('/', function(req, res) {
 /*
  * Johnny-Five code. It is used to control the arduino.
  */
-board.on("ready", function() {
-	console.log('Arduino is ready.');
+// board.on("ready", function() {
+// 	console.log('Arduino is ready.');
 
-	// Change the color of the logo to make it context aware.
-    var temp = new five.Sensor({
-		pin: "A0",
-		// type: "analog",
-		freq: 500
-	});
+// 	// Change the color of the logo to make it context aware.
+//     var temp = new five.Sensor({
+// 		pin: "A0",
+// 		// type: "analog",
+// 		freq: 500
+// 	});
 
-	// Calculate the temperature of the Thermistor based on Steinhart–Hart equation - https://en.wikipedia.org/wiki/Thermistor (the values of A, B and C are given by the manufacturer)
-    calcTemperature = function(R){
-    	A = 0.001129148;
-    	B = 0.000234125;
-    	C = 0.0000000876741;
-    	Rt = Math.log(10000.0 * ((1024.0 / R - 1)));
-		return 1/(A + (B + (C * Rt * Rt )) * Rt);
-	}
+// 	// Calculate the temperature of the Thermistor based on Steinhart–Hart equation - https://en.wikipedia.org/wiki/Thermistor (the values of A, B and C are given by the manufacturer)
+//     calcTemperature = function(R){
+//     	A = 0.001129148;
+//     	B = 0.000234125;
+//     	C = 0.0000000876741;
+//     	Rt = Math.log(10000.0 * ((1024.0 / R - 1)));
+// 		return 1/(A + (B + (C * Rt * Rt )) * Rt);
+// 	}
 
-	// Transform Kelvin to Celsius
-	KtoC = function(dK){
-		return dK - 273.15;
-	}
+// 	// Transform Kelvin to Celsius
+// 	KtoC = function(dK){
+// 		return dK - 273.15;
+// 	}
 
 
-    temp.on("change", function() {	
-    	R = this.raw;
-    	var temperature = calcTemperature(R);
-    	var celsius = KtoC(temperature);
-    	console.log("Celsius:" + celsius);
+//     temp.on("change", function() {	
+//     	R = this.raw;
+//     	var temperature = calcTemperature(R);
+//     	var celsius = KtoC(temperature);
+//     	console.log("Celsius:" + celsius);
 
-    	var colorValue = scale(celsius, 0, 35, 0, 999999);
-    	console.log(colorValue);
+//     	var colorValue = scale(celsius, 0, 35, 0, 999999);
+//     	console.log(colorValue);
     	
-	});
+// 	});
 
 
-	io.on('connection', function(client) {
-		// Client joined.
-	    client.on('join', function(handshake) {
-	      console.log(handshake);
-	    });
+// 	io.on('connection', function(client) {
+// 		// Client joined.
+// 	    client.on('join', function(handshake) {
+// 	      console.log(handshake);
+// 	    });
 
 
     
-  });
-});
+//   });
+// });
 
 const port = process.env.PORT || 3000;
 
