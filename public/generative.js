7722947095e94dc78ceb88d677d287f92d3c979c
Kevin Mamaqi <kevinmamaqi@gmail.com>
2017-12-18 12:29:59 +0100

    added generic file to public folder

--- public/generative.js
+++ public/generative.js
@@ -1,19 +1,3 @@
-(function() {
-
-
-	socket = io.connect('http://localhost:3000');
-
-    socket.on('connect', function(data) {
-        socket.emit('join', 'Client is connected!');
-    });
-
-    socket.on('temperature', function(value){
-      document.getElementById("Layer_2").style.fill = value;
-    });
-
-}());
-
-
 // title: Four to three (sides)
 // author: Eduard Bespalov
 // license: MIT License
